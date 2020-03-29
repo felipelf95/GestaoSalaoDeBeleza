d8613f49298c3a6673c7dc16819a35ee53b8ee71
Felipe <felipe.p95@outlook.com>
2020-03-29 16:38:32 -0300

    created model pessoa and cliente

---
+++ GestaoBeleza/Models/Cliente/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoBeleza.Models.Cliente
+{
+    public class Cliente : Pessoa
+    {
+        public string Instagram { get; set; }
+        public string Facebook { get; set; }
+
+        public Cliente(string nome, string sexo, DateTime dataNascimento, string telefone, string email,string instagram, string facebook, string cpf, string cep, string logradouro, string bairro, string cidade, string estado, string numeroCasa, string complemento)
+            : base(nome,sexo,dataNascimento,telefone,email,cpf,cep,logradouro,bairro,cidade,estado,numeroCasa,complemento)
+        {
+            Instagram = instagram;
+            Facebook = facebook;
+        }
+    }
+}
---
+++ GestaoBeleza/Models/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoBeleza.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string NumeroCasa { get; set; }
+        public string Complemento { get; set; }
+
+        public Pessoa(string nome, string sexo, DateTime dataNascimento, string telefone, string email, string cpf, string cep, string logradouro, string bairro, string cidade, string estado, string numeroCasa, string complemento)
+        {
+            Nome = nome;
+            Sexo = sexo;
+            DataNascimento = dataNascimento;
+            Telefone = telefone;
+            Email = email;
+            Cpf = cpf;
+            Cep = cep;
+            Logradouro = logradouro;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+            NumeroCasa = numeroCasa;
+            Complemento = complemento;
+        }
+    }
+}
